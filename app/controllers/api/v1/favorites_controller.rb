57361582ed415759f54e600fab85f8e6ed29cae3
sbigelo <sean.bigelow1@gmail.com>
2021-06-03 14:22:34 -0400

    added api controller, might remove for something else

---
+++ app/controllers/api/v1/favorites_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::FavoritesController < ApplicationController
+
+    def index
+        favorites = Favorite.all
+        render json: favorites
+    end
+
+    def create
+        favorite = Favorite.new(favorite_params)
+        if favorite.save
+            render json: favorite, status: :accepted
+        else
+            render json: {errors: favorite.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+private
+
+    def favorite_params
+        params.require(:favorite).permit(:supercar, :hypercar, :regular_car, :car_id)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :favorites, only: [:index]
-    end
-  end
+ 
+      resources :favorites
+      # , only: [:index]
+ 
 end
